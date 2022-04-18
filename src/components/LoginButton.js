f279421ff7399c8d4ec33903e6e6659bb19b6c8a
isthidutta <93225111+isthisdutta@users.noreply.github.com>
2022-04-18 04:07:42 +0100

    Fixed Missing Comment

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//imports 'useAuth0' from '@auth0.auth0react' which provides for the login functionality and generates the Login Button
 import { useAuth0 } from '@auth0/auth0-react';
 import "./Log.css";
 
